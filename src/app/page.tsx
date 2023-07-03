2f5f98fa8c0122e6a51e02583d651de8e4c5fd44
Mo. Mirmousavi <mirmousavi.m@gmail.com>
2023-07-03 23:02:48 +0200

    feat: add joy-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+import Button from '@mui/joy/Button';
+
 export default function Home() {
   return (
     <main>
       <h1>My Wall</h1>
 
       <p>Coming soon!</p>
+      <Button variant="solid">Hello World</Button>
     </main>
   )
 }
