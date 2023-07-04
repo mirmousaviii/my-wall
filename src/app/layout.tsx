4ef9da7cb904a99b9637a2b04afa863e9e6cb830
Mo. Mirmousavi <mirmousavi.m@gmail.com>
2023-07-04 03:30:41 +0200

    feat: add lint stage (#7)
    * feat: add lint stage
    
    * chor: husky config
    
    * chor: change lint config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
-import './globals.css'
 import '@fontsource/public-sans';
+
+import './globals.css';
 
 export const metadata = {
   title: 'My Wall',
   description: 'A place to share my thoughts and ideas.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
